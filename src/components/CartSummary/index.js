a5e752365b11131f718f1178f11413711c78b2a8
saisuchal <suchal.sadula528@gmail.com>
2025-01-28 15:53:37 +0000

    code-update

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -26,65 +26,64 @@ const CartSummary = () => (
               </button>
             }
           >
-            {orderSuccess ? (
-              <div className="payment">
+            <div className="payment">
+              <h1>Cart Summary</h1>
+              <p>Total Items (In Qty.): {cartList.length}</p>
+              <p>Amount Payable: Rs.{total}</p>
+              <fieldset className="payment-options">
+                <legend>
+                  <h3>Payment Options</h3>
+                </legend>
+                <div>
+                  <input
+                    type="radio"
+                    name="payment"
+                    id="cardPayment"
+                    disabled
+                  />
+                  <label htmlFor="cardPayment">Card</label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    name="payment"
+                    id="netBankingPayment"
+                    disabled
+                  />
+                  <label htmlFor="netBankingPayment">Net Banking</label>
+                </div>
+                <div>
+                  <input type="radio" name="payment" id="upiPayment" disabled />
+                  <label htmlFor="upiPayment">UPI</label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    name="payment"
+                    id="walletPayment"
+                    disabled
+                  />
+                  <label htmlFor="walletPayment">Wallet</label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    name="payment"
+                    id="codPayment"
+                    onClick={setCod}
+                  />
+                  <label htmlFor="codPayment">Cash on Delivery</label>
+                </div>
+              </fieldset>
+              <button type="button" disabled={!codCheck} onClick={confirmOrder}>
+                Confirm Order
+              </button>
+              {orderSuccess && (
                 <p style={{color: 'green'}}>
                   Your order has been placed successfully
                 </p>
-              </div>
-            ) : (
-              <div className="payment">
-                <h1>Cart Summary</h1>
-                <p>Total Items (In Qty.): {cartList.length}</p>
-                <p>Amount Payable: Rs.{total}</p>
-                <fieldset className="payment-options">
-                  <legend>Payment Options</legend>
-                  <div>
-                    <input
-                      type="radio"
-                      name="payment"
-                      id="netBankingPayment"
-                      disabled
-                    />
-                    <label htmlFor="netBankingPayment">Net Banking</label>
-                  </div>
-                  <div>
-                    <input
-                      type="radio"
-                      name="payment"
-                      id="upiPayment"
-                      disabled
-                    />
-                    <label htmlFor="upiPayment">UPI</label>
-                  </div>
-                  <div>
-                    <input
-                      type="radio"
-                      name="payment"
-                      id="walletPayment"
-                      disabled
-                    />
-                    <label htmlFor="walletPayment">Wallet</label>
-                  </div>
-                  <div>
-                    <input
-                      type="radio"
-                      name="payment"
-                      id="codPayment"
-                      onClick={setCod}
-                    />
-                    <label htmlFor="codPayment">Cash on Delivery</label>
-                  </div>
-                </fieldset>
-                <button
-                  type="button"
-                  disabled={!codCheck}
-                  onClick={confirmOrder}
-                >
-                  Confirm Order
-                </button>
-              </div>
-            )}
+              )}
+            </div>
           </Popup>
         </div>
       )
