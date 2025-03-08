8ba6666e57f2a44019b6376b4e5fde93e3c941f0
saisuchal <suchal.sadula528@gmail.com>
2025-03-08 08:04:55 +0000

    final-update

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -26,77 +26,77 @@ class CartSummary extends Component {
           })
 
           return (
-            <div className="cart-summary-container">
-              <h1 className="order-total-value">
-                <span className="order-total-label">Order Total:</span> Rs{' '}
+            <div className='cart-summary-container'>
+              <h1 className='order-total-value'>
+                <span className='order-total-label'>Order Total:</span> Rs{' '}
                 {total}
                 /-
               </h1>
-              <p className="total-items">{cartList.length} Items in cart</p>
+              <p className='total-items'>{cartList.length} Items in cart</p>
               <Popup
                 modal
                 trigger={
-                  <button type="button" className="checkout-button">
+                  <button type='button' className='checkout-button'>
                     Checkout
                   </button>
                 }
               >
-                <div className="payment">
+                <div className='payment'>
                   <h1 style={{margin: '0px'}}>Cart Summary</h1>
                   <p>Total Items (In Qty.): {cartList.length}</p>
                   <p>Amount Payable: Rs.{total}</p>
-                  <fieldset className="payment-options">
+                  <fieldset className='payment-options'>
                     <legend>
                       <h3>Payment Options</h3>
                     </legend>
                     <div>
                       <input
-                        type="radio"
-                        name="payment"
-                        id="cardPayment"
+                        type='radio'
+                        name='payment'
+                        id='cardPayment'
                         disabled
                       />
-                      <label htmlFor="cardPayment">Card</label>
+                      <label htmlFor='cardPayment'>Card</label>
                     </div>
                     <div>
                       <input
-                        type="radio"
-                        name="payment"
-                        id="netBankingPayment"
+                        type='radio'
+                        name='payment'
+                        id='netBankingPayment'
                         disabled
                       />
-                      <label htmlFor="netBankingPayment">Net Banking</label>
+                      <label htmlFor='netBankingPayment'>Net Banking</label>
                     </div>
                     <div>
                       <input
-                        type="radio"
-                        name="payment"
-                        id="upiPayment"
+                        type='radio'
+                        name='payment'
+                        id='upiPayment'
                         disabled
                       />
-                      <label htmlFor="upiPayment">UPI</label>
+                      <label htmlFor='upiPayment'>UPI</label>
                     </div>
                     <div>
                       <input
-                        type="radio"
-                        name="payment"
-                        id="walletPayment"
+                        type='radio'
+                        name='payment'
+                        id='walletPayment'
                         disabled
                       />
-                      <label htmlFor="walletPayment">Wallet</label>
+                      <label htmlFor='walletPayment'>Wallet</label>
                     </div>
                     <div>
                       <input
-                        type="radio"
-                        name="payment"
-                        id="codPayment"
+                        type='radio'
+                        name='payment'
+                        id='codPayment'
                         onClick={this.setCod}
                       />
-                      <label htmlFor="codPayment">Cash on Delivery</label>
+                      <label htmlFor='codPayment'>Cash on Delivery</label>
                     </div>
                   </fieldset>
                   <button
-                    type="button"
+                    type='button'
                     disabled={!codCheck}
                     onClick={this.confirmOrder}
                   >
